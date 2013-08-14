8c27803a4fe7120ef4eb5af7af2ff674e31c68e3
Yehuda Tuvia <ytuvia@gmail.com>
2013-08-14 16:59:20 +0300

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('website3xApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
