8065ef79251b215785a7d36edb7ed96780c7e0c8
Yehuda Tuvia <ytuvia@gmail.com>
2013-09-09 16:29:42 +0300

    more missing files

---
+++ app/scripts/config/mobliResources.js
@@ -0,0 +1,7 @@
+define(['app'], function(app){
+    return app.constant('mobliResources',
+        {
+            CONFIG_URL: 'http://dev.website3.mobli.com.s3.amazonaws.com/config.json',
+            API_URL: 'http://stage.api.mobli.com/3'
+        });
+});
